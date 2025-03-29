a5fffadc1cb9d738ca6b55b5fea278c4a50f73e1
Digamer001 <diegobezerra864@gmail.com>
2025-03-29 16:11:33 -0300

    Beta 1

--- src/lista-produtos.js
+++ src/lista-produtos.js
@@ -18,6 +18,17 @@ valor: "200-teste",
 genero: "Femea",
 descriçãobasica: "lalalala",
 id:"1"
-}
+},
+{
+    link:'https://shopee.com.br/product/313705796/18530803480?uls_trackid=52bbca9o00v8&utm_campaign=id_KQua7Ez6Eq&utm_content=----&utm_medium=affiliates&utm_source=an_18328410529&utm_term=cs7pnkf5cvvs',
+    imagem:'https://down-br.img.susercontent.com/file/b2eaca95ff94b311bc0172a401a16ea6.webp',
+    item:'Brinquedo Pet 3x1 Mordedor Osso',
+    tipo:'Brinquedo',
+    marca:'Amo Pets',
+    valor:'0',
+    Cores:'verde-azul-vermelho',
+    descrição:'Ele é um produto de ação tripla, ajuda a remover tártaro, massageia gengiva e mantém uma boa higiene bucal do seu amigo pet, quanto mais ele mastiga mais limpo seus dentes ficam.',
+    id:'0001',
+}    
 ];
 
