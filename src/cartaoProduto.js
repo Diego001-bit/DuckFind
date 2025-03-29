fa4f94bd6b2b66019e99c78ef6f37b49d10b053b
Digamer001 <diegobezerra864@gmail.com>
2025-03-29 17:02:04 -0300

    Beta 1

--- src/cartaoProduto.js
+++ src/cartaoProduto.js
@@ -4,8 +4,8 @@ export function renderizarCatalogo(){
     for(const produtoCatalogo of catalogo){
         const cartaoProduto =         
         `<div class="produtos" id="card-produto-${produtoCatalogo.id}">
-        <img src="imagens/${produtoCatalogo.imagem}" class="imagem-item">
-        <div id="pé-cartão">
+        <img src="${produtoCatalogo.imagem}" class="imagem-item">
+        <div class="legenda-produto">
         <p class='productList'>${produtoCatalogo.item}</p>
         <p class='text-sm'>${produtoCatalogo.marca}</p>
         <p class='text-sm'>${produtoCatalogo.valor}</p>
--- src/lista-produtos.js
+++ src/lista-produtos.js
@@ -1,34 +1,24 @@
-export const catalogo=[{
-link: "Teste-link",
-imagem: "teste-imagem",
-item: "nome-produto1",
-tipo: "roupinha-teste",
-marca: "nike",
-valor: "200-teste",
-genero: "Femea",
-descriçãobasica: "lalalala",
-id:"1"
-},{
-link: "Teste-link2",
-imagem: "teste-imagem",
-item: "nome-produto2",
-tipo: "roupinha-teste",
-marca: "nike",
-valor: "200-teste",
-genero: "Femea",
-descriçãobasica: "lalalala",
-id:"1"
-},
-{
-    link:'https://shopee.com.br/product/313705796/18530803480?uls_trackid=52bbca9o00v8&utm_campaign=id_KQua7Ez6Eq&utm_content=----&utm_medium=affiliates&utm_source=an_18328410529&utm_term=cs7pnkf5cvvs',
-    imagem:'https://down-br.img.susercontent.com/file/b2eaca95ff94b311bc0172a401a16ea6.webp',
-    item:'Brinquedo Pet 3x1 Mordedor Osso',
-    tipo:'Brinquedo',
-    marca:'Amo Pets',
-    valor:'0',
-    Cores:'verde-azul-vermelho',
-    descrição:'Ele é um produto de ação tripla, ajuda a remover tártaro, massageia gengiva e mantém uma boa higiene bucal do seu amigo pet, quanto mais ele mastiga mais limpo seus dentes ficam.',
-    id:'0001',
-}    
+export const catalogo = [
+    {
+        link: 'https://shopee.com.br/product/313705796/18530803480?uls_trackid=52bbca9o00v8&utm_campaign=id_KQua7Ez6Eq&utm_content=----&utm_medium=affiliates&utm_source=an_18328410529&utm_term=cs7pnkf5cvvs',
+        imagem: 'https://down-br.img.susercontent.com/file/b2eaca95ff94b311bc0172a401a16ea6.webp',
+        item: 'Brinquedo Pet 3x1 Mordedor Osso',
+        tipo: 'Brinquedo',
+        marca: 'Amo Pets',
+        valor: '0',
+        Cores: 'verde-azul-vermelho',
+        descrição: 'Ele é um produto de ação tripla, ajuda a remover tártaro, massageia gengiva e mantém uma boa higiene bucal do seu amigo pet, quanto mais ele mastiga mais limpo seus dentes ficam.',
+        id: '0001',
+    },{
+        link:'0',
+        imagem:'https://down-br.img.susercontent.com/file/b2eaca95ff94b311bc0172a401a16ea6.webp',
+        Nome:'Brinquedo Pet 3x1 Mordedor Osso',
+        Tipo:'Brinquedo',
+        Marca:'Amo Pets',
+        valor:'0',
+        Cores:'verde-azul-vermelho',
+        descrição:'Ele é um produto de ação tripla, ajuda a remover tártaro, massageia gengiva e mantém uma boa higiene bucal do seu amigo pet, quanto mais ele mastiga mais limpo seus dentes ficam.',
+        id:'0001',
+        }
 ];
 
