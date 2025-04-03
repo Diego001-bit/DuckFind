d9460f9156fdafceabe43882affcd29fa5850d68
Digamer001 <diegobezerra864@gmail.com>
2025-04-02 23:57:45 -0300

    beta 1

--- src/cartaoProduto.js
+++ src/cartaoProduto.js
@@ -6,9 +6,9 @@ export function renderizarCatalogo(){
         `<div class="produtos" id="card-produto-${produtoCatalogo.id}">
         <img src="${produtoCatalogo.imagem}" class="imagem-item">
         <div class="legenda-produto">
-        <p class='productList'>${produtoCatalogo.item}</p>
+        <p class='productList'><strong>${produtoCatalogo.item}</strong></p>
         <p class='text-sm'>Marca: ${produtoCatalogo.material}</p>
-        <p class='text-sm'>${produtoCatalogo.valor}</p>
+        <p class='text-valor'>R$ ${produtoCatalogo.valor}</p>
         <p class='text-sm'>${produtoCatalogo.Cores}</p>
         <a href="${produtoCatalogo.link}" target="_blank"><div class="botao-afiliado"><span class="material-symbols-outlined">shopping_cart </span></div></a>
         </div>
