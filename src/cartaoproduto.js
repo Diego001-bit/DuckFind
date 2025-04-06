ab685c88a7193f95c990886890cb0cced8327851
Digamer001 <diegobezerra864@gmail.com>
2025-04-06 10:01:47 -0300

    beta 1

--- src/cartaoproduto.js
+++ src/cartaoproduto.js
@@ -6,11 +6,10 @@ export function renderizarcatalogo(){
         `<div class="produtos" id="card-produto-${produtoCatalogo.id}">
         <img src="${produtoCatalogo.imagem}" class="imagem-item">
         <div class="legenda-produto">
-        <p class='productList'><strong>${produtoCatalogo.item}</strong></p>
-        <p class='text-sm'>Marca: ${produtoCatalogo.material}</p>
-        <p class='text-valor'>R$ ${produtoCatalogo.valor}</p>
+        <p class='productList'><strong>${produtoCatalogo.Nome}</strong></p>
+        <p class='text-sm'>Material: ${produtoCatalogo.Material}</p>
         <p class='text-sm'>${produtoCatalogo.Cores}</p>
-        <a href="${produtoCatalogo.link}" target="_blank"><div class="botao-afiliado"><span class="material-symbols-outlined">shopping_cart </span></div></a>
+        <a href="${produtoCatalogo.link}" target="_blank"><div class="botao-afiliado"><span class="material-symbols-outlined">visibility</span></div></a>
         </div>
         </div>`;
         document.getElementById('container-produto').innerHTML += cartaoProduto;
--- src/lista-produtos.js
+++ src/lista-produtos.js
@@ -1,14 +1,125 @@
 export const catalogo = [
     {
-        link:'https://shopee.com.br/product/313705796/18530803480?uls_trackid=52bbca9o00v8&utm_campaign=id_KQua7Ez6Eq&utm_content=----&utm_medium=affiliates&utm_source=an_18328410529&utm_term=cs7pnkf5cvvs',
-        imagem:'https://down-br.img.susercontent.com/file/b2eaca95ff94b311bc0172a401a16ea6.webp',
-        item: 'Brinquedo Pet 3x1 Mordedor Osso',
-        tipo: 'Brinquedo',
-        material: 'Amo Pets',
-        valor: '0',
-        Cores: 'verde-azul-vermelho',
-        descrição: 'Ele é um produto de ação tripla, ajuda a remover tártaro, massageia gengiva e mantém uma boa higiene bucal do seu amigo pet, quanto mais ele mastiga mais limpo seus dentes ficam.',
-        id: '0001',
-    }
+        link:'https://s.shopee.com.br/60Dzp4L10A',
+        imagem:'https://down-br.img.susercontent.com/file/br-11134207-7r98o-lzgporbxq1j50a.webp',
+        Nome:'Coleira Peitoral Ajustável Refletivo',
+        Tipo:'Coleira',
+        Material:'Poliéster',
+        valor:'0',
+        Cores:'Azul-vermelho-Rosa-Preto',
+        descrição:'A Coleira Peitoral para Cães e Gatos é um produto de qualidade e conforto para o seu animal de estimação. Fabricada com materiais duráveis e resistentes, garante segurança e bem-estar para o seu pet.',
+        id:'0001',
+        },{
+            link:'https://s.shopee.com.br/3fq52oLWnn',
+            imagem:'https://down-br.img.susercontent.com/file/br-11134207-7r98o-lryyk293wnjv55.webp',
+            Nome:'Gato BOLA Brinquedo Elétrico Automático',
+            Tipo:'Brinquedo',
+            Material:'ABS + Silicone ',
+            valor:'0',
+            Cores:'Azul-Laranja-Rosa',
+            descrição:'Brinquedo interativo para gatos Smart, muito interessante, carregamento USB, rolamento inteligente, ótimo brinquedo para pet.',
+            id:'0002',
+            },{
+                link:'https://s.shopee.com.br/6KqqDja8K7',
+                imagem:'https://down-br.img.susercontent.com/file/br-11134207-7r98o-m1pirl9if9w175.webp',
+                Nome:'Grade Portão Proteção Pet Cachorro',
+                Tipo:'0',
+                Material:'Aço',
+                valor:'0',
+                Cores:'Preto-Branco',
+                descrição:'Indicado para: cães de pequeno, médio porte, e também para crianças.Não possui cantos vivos na estrutura o que promove mais segurança a seu bebe.',
+                id:'0003',
+                },{
+                    link:'https://s.shopee.com.br/6pn6ofgcpT',
+                    imagem:'https://down-br.img.susercontent.com/file/sg-11134201-7rd5v-lv93uz2o2qgl3a@resize_w450_nl.webp',
+                    Nome:'Kit Brinquedo Mordedor para Cachorro Pneu G',
+                    Tipo:'Brinquedo',
+                    Material:'Borracha',
+                    valor:'0',
+                    Cores:'Preto',
+                    descrição:'Estimule o desenvolvimento e fortalecimento dos dentes do seu cão enquanto ele se diverte!',
+                    id:'0004',
+                    },{
+                        link:'https://s.shopee.com.br/AUgPBRxtUk',
+                        imagem:'https://down-br.img.susercontent.com/file/br-11134207-7r98o-m0eo53tdf80t56.webp',
+                        Nome:'kit Rolo Filme Anti Arranhão Protetor Adesivo',
+                        Tipo:'Protedor',
+                        Material:'Plástico PVC',
+                        valor:'0',
+                        Cores:'Transparente',
+                        descrição:'Não espere mais para proteger seus móveis e manter sua casa impecável! Adquira agora mesmo nosso rolo Adesivo Autocolante Protetor contra Arranhões de Gatos e Cachorros e desfrute de uma convivência harmoniosa com seus animais de estimação.',
+                        id:'0005',
+                        },{
+                            link:'https://s.shopee.com.br/2B1HG9tc3O',
+                            imagem:'https://down-br.img.susercontent.com/file/br-11134207-7r98o-m5ww094f21s51b.webp',
+                            Nome:'Comedouro de Gato em Madeira e tigela de Alumínio',
+                            Tipo:'Comedouro',
+                            Material:'Madeira Pinus',
+                            valor:'0',
+                            Cores:'Comedouro: Individual,Duplo e Triplo',
+                            descrição:'Nossos comedouros são desenvolvidos e projetados para ajudar seu pet na hora da alimentação, feito em Madeira Pinus de ótima qualidade de reflorestamento combina com qualquer ambiente.',
+                            id:'0006',
+                            },{
+                                link:'https://s.shopee.com.br/1VlaSxHkHD',
+                                imagem:'https://down-br.img.susercontent.com/file/br-11134207-7r98o-lwzqi9599z27aa.webp',
+                                Nome:'Casinha Gato Tóca Rampa Casa Caminha Pet',
+                                Tipo:'Casa',
+                                Material:'Madeira',
+                                valor:'0',
+                                Cores:'Azul-Cinza-Bege',
+                                descrição:'Esta Caminha foi projetada pensando no conforto e na segurança do seu pet.',
+                                id:'0007',
+                                },{
+                                    link:'https://s.shopee.com.br/30aOFjc1sh',
+                                    imagem:'https://down-br.img.susercontent.com/file/sg-11134201-7rdw5-m18s5k09cflx17@resize_w450_nl.webp',
+                                    Nome:'Rolo Tira Pelos Lavável 2 Peças Roupas',
+                                    Tipo:'Utensílio',
+                                    Material:'PP, TPR e SEBS',
+                                    valor:'0',
+                                    Cores:'Amarelo, Azul, Rosa e Roxo',
+                                    descrição:'O Rolo Adesivo Lavavel e o que faltava para que voce limpe suas roupas, sofas ou tecidos com mais facilidade e praticidade. Se voce possui animais que soltam pelos nao pode ficar sem esse produto maravilhoso.',
+                                    id:'0008',
+                                    },{
+                                        link:'https://s.shopee.com.br/30aOFkkQOw',
+                                        imagem:'https://down-br.img.susercontent.com/file/d86b2ce84d6ab04bf30ed1b0dec43da9.webp',
+                                        Nome:'Brinquedo Pet 3x1 Mordedor Osso Massageador',
+                                        Tipo:'Brinquedo',
+                                        Material:'Borracha (nylon)',
+                                        valor:'0',
+                                        Cores:'Verde-Vermelho-Azul',
+                                        descrição:'Ele é um produto de ação tripla, ajuda a remover tártaro, massageia gengiva e mantém uma boa higiene bucal do seu amigo pet, quanto mais ele mastiga mais limpo seus dentes ficam.',
+                                        id:'0009',
+                                        },{
+                                            link:'https://s.shopee.com.br/8fEl0BtFLD',
+                                            imagem:'https://down-br.img.susercontent.com/file/br-11134207-7r98o-m2cunishu8hdfb.webp',
+                                            Nome:'Caminha Pet - Caminha de Cachorro Cama Pet Luxo',
+                                            Tipo:'Cama',
+                                            Material:'0',
+                                            valor:'0',
+                                            Cores:'Preto-Vermelho-Azul-Rosa',
+                                            descrição:'la irá proporcionar ao seu amiguinho 100% de conforto. a caminha é muito macia, bem costurada e resistente também possui fundo impermeável, evitando passar umidade e friagem para o seu animalzinho',
+                                            id:'0010',
+                                            },{
+                                                link:'https://s.shopee.com.br/qVtfoucjN',
+                                                imagem:'https://down-br.img.susercontent.com/file/br-11134207-7r98o-lvp7h6pbu59o0a@resize_w450_nl.webp',
+                                                Nome:'Arranhador Para Gatos Poste',
+                                                Tipo:'Brinquedo',
+                                                Material:'Base em madeira MDF',
+                                                valor:'0',
+                                                Cores:'Rosa-Preto-Marrom-Creme-Cinza-Azul-Vermelho',
+                                                descrição:'Este arranhador é projetado para garantir horas de entretenimento para o seu gato. O formato quadrado oferece estabilidade, enquanto os brinquedos artesanais, incluindo pompons com penas, garantem atração instantânea para o seu pet.',
+                                                id:'0011',
+                                                }
+                                                
+                                            
+                                        
+                                    
+                                
+                            
+                        
+                    
+                
+            
+        
 ];
 
